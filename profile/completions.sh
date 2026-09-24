4aa1e4f16dbc99466147219f415adf81a317232e
calamity-m <11038569+calamity-m@users.noreply.github.com>
2026-09-25 07:22:07 +1000

    feat(completions): add fzf

--- profile/completions.sh
+++ profile/completions.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+export FZF_COMPLETION_TRIGGER=','
+
+
 eval "$(mise activate bash)"
 eval $(gh completion -s bash)
 eval "$(peanutbutter completions bash)"
+eval "$(fzf --bash)"
